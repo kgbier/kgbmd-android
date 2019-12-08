8a59402f0b4295c6807eff4d36597834cf9fda97
Konrad Biernacki <kgbier@gmail.com>
2019-12-08 17:59:56 +1100

    Refactor MainActivity

--- app/src/main/java/com/kgbier/kgbmd/MainActivity.kt
+++ app/src/main/java/com/kgbier/kgbmd/MainActivity.kt
@@ -30,32 +30,30 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope(), Lifecyc
         rootView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
 
-        val routeOrdinal =
-            savedInstanceState?.getInt(KEY_NAV_CURRENT_ROUTE, ROUTE_UNDEFINED) ?: ROUTE_UNDEFINED
-
         savedInstanceState?.getIntArray(KEY_NAV_BACK_STACK)?.map {
             Route.getRoute(RouteId.values()[it])
         }?.let {
             backStack.addAll(it)
         }
 
-        val route = if (routeOrdinal == ROUTE_UNDEFINED) {
-            Route.MainPosterScreen
-        } else {
-            Route.getRoute(RouteId.values()[routeOrdinal])
-        }
+        val route = savedInstanceState?.getInt(KEY_NAV_CURRENT_ROUTE, ROUTE_UNDEFINED)?.takeUnless {
+            it == ROUTE_UNDEFINED
+        }?.let {
+            Route.getRoute(RouteId.values()[it])
+        } ?: Route.MainPosterScreen
+
         showScreen(route)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt(
-            KEY_NAV_CURRENT_ROUTE,
-            currentRoute?.id?.ordinal ?: ROUTE_UNDEFINED
-        )
         outState.putIntArray(
             KEY_NAV_BACK_STACK,
             backStack.map { it.id.ordinal }.toIntArray()
+        )
+        outState.putInt(
+            KEY_NAV_CURRENT_ROUTE,
+            currentRoute?.id?.ordinal ?: ROUTE_UNDEFINED
         )
     }
 
@@ -64,10 +62,9 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope(), Lifecyc
         showScreen(route)
     }
 
-    fun navigateBack() {
-        if (backStack.isEmpty()) {
-            finish(); return
-        }
+    fun navigateBack() = if (backStack.isEmpty()) {
+        finish()
+    } else {
         showScreen(backStack.pop())
     }
 
