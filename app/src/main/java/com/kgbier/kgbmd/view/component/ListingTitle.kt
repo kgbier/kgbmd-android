d0c571a24c1c28894e5910906167928fb26633f8
Konrad Biernacki <kgbier@gmail.com>
2020-08-09 20:01:00 +1000

    Make listing title a component

---
+++ app/src/main/java/com/kgbier/kgbmd/view/component/ListingTitle.kt
@@ -0,0 +1,48 @@
+package com.kgbier.kgbmd.view.component
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.toSpannable
+import com.kgbier.kgbmd.R
+import com.kgbier.kgbmd.util.resolveAttribute
+import com.kgbier.kgbmd.util.setTextColorAttr
+import com.kgbier.kgbmd.util.setTextStyleAttr
+
+@SuppressLint("ViewConstructor")
+class ListingTitle(context: Context) : AppCompatTextView(context) {
+    init {
+        setTextStyleAttr(R.attr.textAppearanceHeadline5)
+        setTextColorAttr(android.R.attr.textColorPrimary)
+    }
+
+    fun setTitleSequence(name: String, subscript: String?) {
+        text = SpannableStringBuilder().apply {
+            append(name)
+            subscript?.let { _ ->
+                val subtitleStyleSpan = TextAppearanceSpan(
+                    context,
+                    R.style.TextAppearance_MaterialComponents_Subtitle1
+                )
+                val secondaryColorSpan =
+                    resolveAttribute(android.R.attr.textColorSecondary)?.let {
+                        ResourcesCompat.getColor(resources, it, context.theme)
+                    }?.let {
+                        ForegroundColorSpan(it)
+                    }
+
+                append("\u00A0") // non-breaking space
+                setSpan(subtitleStyleSpan, length, length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+                secondaryColorSpan?.let {
+                    setSpan(it, length, length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+                }
+                append("($subscript)")
+            }
+        }.toSpannable()
+    }
+}
--- app/src/main/java/com/kgbier/kgbmd/view/ui/listingdetails/TitleView.kt
+++ app/src/main/java/com/kgbier/kgbmd/view/ui/listingdetails/TitleView.kt
@@ -1,65 +1,22 @@
 package com.kgbier.kgbmd.view.ui.listingdetails
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.ForegroundColorSpan
-import android.text.style.TextAppearanceSpan
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.text.toSpannable
-import com.kgbier.kgbmd.R
-import com.kgbier.kgbmd.util.resolveAttribute
-import com.kgbier.kgbmd.util.setTextColorAttr
-import com.kgbier.kgbmd.util.setTextStyleAttr
-
-@SuppressLint("ViewConstructor")
-class TitleView(context: Context) : AppCompatTextView(context) {
-    init {
-        setTextStyleAttr(R.attr.textAppearanceHeadline5)
-        setTextColorAttr(android.R.attr.textColorPrimary)
-    }
-}
+import com.kgbier.kgbmd.view.component.ListingTitle
 
 class TitleListingViewModel(val name: String, val yearReleased: String?) : BaseListingViewModel
 
-class TitleListingViewHolder(context: Context) : BaseListingViewHolder(TitleView(context).apply {
+class TitleListingViewHolder(context: Context) : BaseListingViewHolder(ListingTitle(context).apply {
     layoutParams = ViewGroup.LayoutParams(
         ViewGroup.LayoutParams.MATCH_PARENT,
         ViewGroup.LayoutParams.WRAP_CONTENT
     )
 }) {
-    val view get() = itemView as TitleView
+    val view get() = itemView as ListingTitle
 
     override fun bind(viewModel: BaseListingViewModel) {
         if (viewModel !is TitleListingViewModel) return
 
-        val titleSequence = SpannableStringBuilder().apply {
-            append(viewModel.name)
-            viewModel.yearReleased?.let { yearReleased ->
-                val subtitleStyleSpan =
-                    TextAppearanceSpan(
-                        view.context,
-                        R.style.TextAppearance_MaterialComponents_Subtitle1
-                    )
-                val secondaryColorSpan =
-                    view.resolveAttribute(android.R.attr.textColorSecondary)?.let {
-                        ResourcesCompat.getColor(view.resources, it, view.context.theme)
-                    }?.let {
-                        ForegroundColorSpan(it)
-                    }
-
-                append("\u00A0") // non-breaking space
-                setSpan(subtitleStyleSpan, length, length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                secondaryColorSpan?.let {
-                    setSpan(it, length, length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                }
-                append("($yearReleased)")
-            }
-        }.toSpannable()
-
-        view.text = titleSequence
+        view.setTitleSequence(viewModel.name, viewModel.yearReleased)
     }
 }
